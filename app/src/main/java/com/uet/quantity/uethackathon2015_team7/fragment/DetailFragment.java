26f0593fa6f632e6db75aed990a49569606f329c
uethackathon07 <uethackathon.team07@gmail.com>
2015-11-22 01:25:59 +0700

    Update detail Screen

--- app/src/main/java/com/uet/quantity/uethackathon2015_team7/fragment/DetailFragment.java
+++ app/src/main/java/com/uet/quantity/uethackathon2015_team7/fragment/DetailFragment.java
@@ -1,13 +1,20 @@
 package com.uet.quantity.uethackathon2015_team7.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.uet.quantity.uethackathon2015_team7.R;
+
+import java.io.InputStream;
 
 /**
  * Created by James Crabby on 11/20/2015.
@@ -15,6 +22,7 @@ import com.uet.quantity.uethackathon2015_team7.R;
 public class DetailFragment extends Fragment {
 
     public static DetailFragment instance;
+    private final String URL = "http://uethackathon07.herokuapp.com/api/pictures/21_11";
 
     public static DetailFragment newInstance() {
         if(instance == null) {
@@ -27,6 +35,34 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_detail, container, false);
+        ImageView img_infographic = (ImageView) v.findViewById(R.id.img_infographic);
+        new DownloadImageTask(img_infographic).execute(URL);
+
         return v;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
